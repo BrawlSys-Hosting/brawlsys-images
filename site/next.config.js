9987e10520b387a639423c2bad01ba2add632da0
Eli F <140041033+lordsmeef@users.noreply.github.com>
2024-09-16 12:39:16 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    name: 'BrawlSys Hosting',
+    description: 'The official store for BrawlSys Maintained Kasm images.',
+    icon: 'https://registry.brawlsys.us/1.0/image.png',
+    listUrl: 'https://registry.brawlsys.us/',
+    contactUrl: 'https://github.com/BrawlSys-Hosting/brawlsys-images/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
